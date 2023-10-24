ca1e6a4cb357c84ec3ef55bb11e8c50173f483af
Norbok7 <72887701+Norbok7@users.noreply.github.com>
2023-10-24 14:31:16 -0500

    added functionality to difficulty levels
    need to implement scale of difficulty into buttons may require more refactoring

--- Exercise-Plan-Class-Project/src/app/difficulty-level/difficulty-level.component.ts
+++ Exercise-Plan-Class-Project/src/app/difficulty-level/difficulty-level.component.ts
@@ -6,12 +6,12 @@ import { ExerciseListComponent } from "../exercise-list/exercise-list.component"
 @Component({
   selector: 'app-difficulty-level',
   templateUrl: './difficulty-level.component.html',
-  styleUrls: ['./difficulty-level.component.css']
+  styleUrls: ['./difficulty-level.component.css'],
 })
 export class DifficultyLevelComponent implements OnInit {
-  coreArray: Exercise[];
-  lowerArray: Exercise[];
-  upperArray: Exercise[];
+  coreExercises: Exercise[];
+  lowerExercises: Exercise[];
+  upperExercises: Exercise[];
 
   constructor(private exerciseService: ExerciseService, private exList: ExerciseListComponent,
     // private router: Router, //used to route inside of typescript
@@ -20,116 +20,31 @@ export class DifficultyLevelComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
         const diffLvl = +params['difficulty'];
-    })}
+    })
+  }
 
 
 
-//   onBegUpperBodyWorkout(){
-// this.exList.upper();
-// this.exerciseService.myExercises
-// console.log(this.exerciseService.myExercises);
-//     // parent.addEventListener('click', this.onBegUpperBodyWorkout);
-//     // document.getElementById('e').innerHTML = ' <br><br><br><br><br>For an beginner workout try 2-3 sets of 8-10 repetitions.  Make sure to ask questions, or look things up to make sure you are comfortable.'
-//     // document.getElementById('upper').style.display='block'
-//     // document.getElementById('lower').style.display='none'
-//     // document.getElementById('core').style.display='none'
 
-//     }
-//     onBegLowerBodyWorkout(){
-// this.exList.lower();
-//       // parent.addEventListener('click', this.onBegLowerBodyWorkout);
-//       // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an beginner workout try 2-3 sets of 8-10 repetitions.  Make sure to ask questions, or look things up to make sure you are comfortable.'
-//       // document.getElementById('lower').style.display='block'
-//       // document.getElementById('upper').style.display='none'
-//       // document.getElementById('core').style.display='none'
-
-//     }
-//     onBegCoreWorkout(){
-// this.exList.core();
-//       // parent.addEventListener('click', this.onBegCoreWorkout);
-//       // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an beginner workout try 2-3 sets of 8-10 repetitions.  Make sure to ask questions, or look things up to make sure you are comfortable.'
-//       // document.getElementById('core').style.display='block'
-//       // document.getElementById('lower').style.display='none'
-//       // document.getElementById('upper').style.display='none'
-
-
-//     }
-//     onItermUpperBodyWorkout(){
-//       this.exList.upper();
-//       // parent.addEventListener('click', this.onItermUpperBodyWorkout);
-//       // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an intermediate workout try 3-4 sets of 8-12 repetitions. The biggest difference between you and advanced lifter is the number of sets you do!'
-//       // document.getElementById('upper').style.display='block'
-//       // document.getElementById('lower').style.display='none'
-//       // document.getElementById('core').style.display='none'
-
-
-//       }
-//       onItermLowerBodyWorkout(){
-//         this.exList.lower();
-//         // parent.addEventListener('click', this.onItermLowerBodyWorkout);
-//         // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an intermediate workout try 3-4 sets of 8-12 repetitions. The biggest difference between you and advanced lifter is the number of sets you do!'
-//         // document.getElementById('lower').style.display='block'
-//         // document.getElementById('upper').style.display='none'
-//         // document.getElementById('core').style.display='none'
-
-//       }
-//       onItermCoreWorkout(){
-//         this.exList.core();
-//         // parent.addEventListener('click', this.onItermCoreWorkout);
-//         // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an intermediate workout try 3-4 sets of 8-12 repetitions. The biggest difference between you and advanced lifter is the number of sets you do!'
-//         // document.getElementById('core').style.display='block'
-//         // document.getElementById('lower').style.display='none'
-//         // document.getElementById('upper').style.display='none'
-
-//       }
-//     onAdvUpperBodyWorkout(){
-//       this.exList.upper();
-//       // parent.addEventListener('click', this.onAdvUpperBodyWorkout);
-//       // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an advanced workout try 4-5 sets of 8-12 repetitions, also increase weight by 5 pounds or 5% until near failure is achieved by repitition 12.'
-//       // document.getElementById('upper').style.display='block'
-//       // document.getElementById('lower').style.display='none'
-//       // document.getElementById('core').style.display='none'
-
-
-//   }
-//   onAdvLowerBodyWorkout(){
-//     this.exList.lower();
-//     // parent.addEventListener('click', this.onAdvLowerBodyWorkout);
-//     // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an advanced workout try 4-5 sets of 8-12 repetitions, also increase weight by 5 pounds or 5% until near failure is achieved by repitition 12.'
-//     // document.getElementById('lower').style.display='block'
-//     // document.getElementById('upper').style.display='none'
-//     // document.getElementById('core').style.display='none'
-
-//   }
-//   onAdvCoreWorkout(){
-//     this.exList.core();
-//     // parent.addEventListener('click', this.onAdvCoreWorkout);
-//     // document.getElementById('e').innerHTML = '<br><br><br><br><br>For an advanced workout try 4-5 sets of 8-12 repetitions, also increase weight by 5 pounds or 5% until near failure is achieved by repitition 12.'
-//     // document.getElementById('core').style.display='block'
-//     // document.getElementById('lower').style.display='none'
-//     // document.getElementById('upper').style.display='none'
-
-//   }
   core() {
-    let filteredExercises = this.exerciseService.myExercises.filter(
+    this.coreExercises = this.exerciseService.myExercises.filter(
       (exercises) => exercises.bodypart === BodyPart.core
     );
-    return filteredExercises
+
 
   }
 
   lower() {
-    // return this.filteredExercises;
-    let filteredExercises = this.exerciseService.myExercises.filter(
+    this.lowerExercises = this.exerciseService.myExercises.filter(
       (exercises) => exercises.bodypart === BodyPart.lower
     );
-    return filteredExercises
+
   }
   upper() {
-    // return this.filteredExercises;
-    let filteredExercises = this.exerciseService.myExercises.filter(
+    this.upperExercises = this.exerciseService.myExercises.filter(
       (exercises) => exercises.bodypart === BodyPart.upper
     );
-    return filteredExercises
+
   }
 }
+
